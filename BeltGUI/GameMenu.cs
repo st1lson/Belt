26a8407b87125cac11cfe27ef798792f3f0a10b2
Max <rendon4ik@gmail.com>
2021-12-07 23:42:51 +0200

    Added resources for GUI

--- BeltGUI/GameMenu.cs
+++ BeltGUI/GameMenu.cs
@@ -1,12 +1,37 @@
-﻿using System.Windows.Forms;
+﻿using System.Collections;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Windows.Forms;
+using BeltGUI.Properties;
 
-namespace BeltUI
+namespace BeltGUI
 {
     public partial class GameMenu : Form
     {
+        private readonly Bitmap[] _sprites;
         public GameMenu()
         {
             InitializeComponent();
+            _sprites = InitializeSprites();
+        }
+
+        private Bitmap[] InitializeSprites()
+        {
+            Bitmap[] sprites = new Bitmap[53];
+            ResourceSet resourceSet = Resources.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, false, true);
+            if (resourceSet is null)
+            {
+                return default;
+            }
+
+            int i = 0;
+            foreach (DictionaryEntry entry in resourceSet)
+            {
+                _sprites[i++] = (Bitmap)entry.Value;
+            }
+
+            return sprites;
         }
     }
 }
